465260e5315de54ce98d41e7c7e077c8d3041d6c
Flugstein <flugstein@pm.me>
2020-04-06 16:29:33 +0200

    add threading

--- README.md
+++ README.md
@@ -3,8 +3,7 @@ A simple speech to text to speech program that enables you to talk without revea
 
 ## Setup
 - Tested only on Windows
-- Install Python packages `SpeechRecognition` `PyAudio` `gtts` `playsound`. Using conda (miniconda or anaconda) is recommend to install PyAudio since pip cannot install the dependency PortAudio on it's own.
-- Make an account at https://wit.ai and put the token into `wit_token.txt`.
+- Install Python packages `SpeechRecognition` `PyAudio` `gtts` `playsound` `pydub`. Using conda (miniconda or anaconda) is recommend to install PyAudio since pip cannot install the dependency PortAudio on it's own.
 
 ## Run
 Run `moe.py` and start speaking after the first beep. After you stop talking the second beep marks the ending of the recording.
--- moe.py
+++ moe.py
@@ -4,31 +4,95 @@ from playsound import playsound
 from pydub import AudioSegment
 from pydub.playback import play
 from io import BytesIO
+import subprocess
+from threading import Thread
+from queue import Queue
+import signal
+import sys
 
-# speech to text
-with open('wit_token.txt', 'r') as f:
-  wit_token = f.read()
-r = sr.Recognizer()
-mic = sr.Microphone()
 
-with mic as source:
-    r.adjust_for_ambient_noise(source)
-    print('Speak now...')
-    playsound('ready.wav')
-    audio = r.listen(source)
-    playsound('done.wav')
-    print('Got it')
+def queue_record(recognizer, microphone, record_queue):
+    id = 0
+    while True:
+        with microphone as source:
+            recognizer.adjust_for_ambient_noise(source)
+            print('{} - Recording...'.format(id))
+            record = recognizer.listen(source)
+            print('{} - Got it'.format(id))
 
-# text = r.recognize_wit(audio, wit_token)
-text = r.recognize_google(audio)
-print('You said: "{}"'.format(text))
+        record_dict = {'id': id, 'record': record}
+        record_queue.put_nowait(record_dict)
 
-# text to speech
-speech = gTTS(text, lang='it')
+        id += 1
 
-# mp3_file = 'speech.mp3'
-# speech.save(mp3_file)
-mp3_fp = BytesIO()
-speech.write_to_fp(mp3_fp)
-mp3_fp.seek(0)
-play(AudioSegment.from_file(mp3_fp, format="mp3"))
+def queue_record_to_text_thread(record_queue, text_queue):
+    while True:
+        record_dict = record_queue.get()
+        if record_dict:
+            id = record_dict['id']
+            record = record_dict['record']
+
+            print('{} - Generating text...'.format(id))
+            try:
+                text = recognizer.recognize_google(record)
+                print('{} - You said: "{}"'.format(id, text))
+
+                text_dict = {'id': id, 'text': text}
+                text_queue.put_nowait(text_dict)
+
+            except sr.RequestError:
+                print('{} - Error: API unavailable'.format(id))
+            except sr.UnknownValueError:
+                print('{} - Error: Unable to recognize speech'.format(id))
+
+        record_queue.task_done()
+
+def queue_text_to_playback_thread(text_queue, playback_queue):
+    while True:
+        text_dict = text_queue.get()
+        if text_dict:
+            id = text_dict['id']
+            text = text_dict['text']
+
+            print('{} - Generating playback for: "{}"'.format(id, text))
+            speech = gTTS(text, lang='it')
+            mp3_fp = BytesIO()
+            speech.write_to_fp(mp3_fp)
+            mp3_fp.seek(0)
+
+            playback_dict = {'id': id, 'text': text, 'playback': mp3_fp}
+            playback_queue.put_nowait(playback_dict)
+
+        text_queue.task_done()
+
+def queue_playback_thread(playback_queue):
+    while True:
+        playback_dict = playback_queue.get()
+        if playback_dict:
+            id = playback_dict['id']
+            text = playback_dict['text']
+            playback = playback_dict['playback']
+
+            print('{} - Playback: "{}"'.format(id, text))
+            play(AudioSegment.from_file(playback, format='mp3'))
+
+        playback_queue.task_done()
+
+
+if __name__ == '__main__':
+
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    record_queue = Queue(maxsize=0)
+    text_queue = Queue(maxsize=0)
+    playback_queue = Queue(maxsize=0)
+
+    record_thread = Thread(target=queue_record, args=[recognizer, microphone, record_queue])
+    record_thread.start()
+    record_to_text_thread = Thread(target=queue_record_to_text_thread, args=[record_queue, text_queue])
+    record_to_text_thread.start()
+    text_to_playback_thread = Thread(target=queue_text_to_playback_thread, args=[text_queue, playback_queue])
+    text_to_playback_thread.start()
+    playback_thread = Thread(target=queue_playback_thread, args=[playback_queue])
+    playback_thread.start()
